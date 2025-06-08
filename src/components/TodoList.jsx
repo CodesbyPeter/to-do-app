f3d3b706eabaeeef098c535fef2a567729d8db60
CodesByPeter <petermusumba360@gmail.com>
2025-06-08 10:44:47 +0300

    Creating the todoList basic functional setup.

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoItem from "./TodoItem";  // Import TodoItem component
+
+function TodoList({ todos, toggleTodo, deleteTodo }) {
+    return (
+        <ul>
+
+        </ul>
+    );
+}  
+
+export default TodoList;  // Export TodoList component
