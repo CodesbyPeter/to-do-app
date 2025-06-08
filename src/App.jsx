343329aabe45544bc26e94b4c72ba46e8d5953a3
CodesByPeter <petermusumba360@gmail.com>
2025-06-08 10:05:17 +0300

    Added the placeholder element through jsx where one can add a task and also search for a task.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import Header from "./components/Header";
 import React, { useState } from "react";
+import TodoInput from "./components/TodoInput";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { id: Date.now(), text, completed: false }]);
+  };
 
   const toggleDarkMode = () => setDarkMode(prev => !prev);
 
   return (
     <div>
       <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <TodoInput addTodo={addTodo} />
     </div>
   );
 }
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function TodoInput({ addTodo}) {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();      // Prevents page reload
+        if (!inputValue.trim()) return;    // Ignores empty inputs
+            addTodo(inputValue);   // Calls the addTodo function with the input value
+            setInputValue("");     // Clears the input field
+        }
+
+    const [inputValue, setInputValue] = useState("");
+    return (
+        <form onSubmit={handleSubmit}>
+          <input 
+          type="text"
+          value={inputValue}
+          onChange={ (e) => setInputValue(e.target.value) }
+          placeholder="Add a new task...."
+          onKeyPress={ (e) => {
+            if (e.key === 'Enter') {
+                handleSubmit(e); }
+          }}/>
+          <button type="submit">+</button>
+        </form>
+    );
+}
+
+export default TodoInput;
